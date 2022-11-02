11015c5079ae8d285a288351e36c9033f11a151e
Mocren <Kl.Artur@web.de>
2022-11-02 12:16:00 +0100

    Added calculation of distance

--- Uebung2-Datentypen_Operatoren/src/aufgabe6/Main.java
+++ Uebung2-Datentypen_Operatoren/src/aufgabe6/Main.java
@@ -10,7 +10,7 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 
 		String operation;
-		int erg = 0;
+		double erg = 0;
 
 		System.out.println("Welche Rechenoperation möchten Sie durchführen?");
 		System.out.println("Die Auswahl treffen Sie mit A=Addition, S=Subtraktion, D=Durchschnitt, AS=Abstand ");
@@ -18,9 +18,10 @@ public class Main {
 
 		switch (operation) {
 		case "A": {
-			int val1;
-			int val2;
+			int val1, val2;
+
 			System.out.println("Addition:");
+			
 			System.out.println("Bitte geben Sie die erste Zahl ein.");
 			val1 = scan.nextInt();
 			System.out.println("Bitte geben sie die zweite Zahl ein.");
@@ -30,9 +31,10 @@ public class Main {
 
 		}
 		case "S": {
-			int val1;
-			int val2;
+			int val1, val2;
+
 			System.out.println("Subtraktion:");
+			
 			System.out.println("Bitte geben Sie die erste Zahl ein.");
 			val1 = scan.nextInt();
 			System.out.println("Bitte geben sie die zweite Zahl ein.");
@@ -44,6 +46,8 @@ public class Main {
 		case "D": {
 			ArrayList<Integer> arr = new ArrayList<>();
 			int input = -1;
+			int fullVal = 0;
+
 			System.out.println("Durchschnittsberechnung");
 			System.out.println("Bitte geben Sie die Zahlen ein, dessen Durchschnitt Sie berechnen wollen.");
 			System.out.println("Mit der Eingabe von 0 beenden Sie die Zahlensammlung.");
@@ -53,23 +57,33 @@ public class Main {
 				input = scan.nextInt();
 				arr.add(input);
 			}
-			
-			int fullVal = 0;
-			
+
 			for (int i = 0; i < arr.size(); i++) {
 				fullVal += arr.get(i);
 			}
-			
-			erg = fullVal / (arr.size() -1) ;
+
+			erg = fullVal / (arr.size() - 1);
 
 		}
 		case "AS": {
+			System.out.println("Abstandsberechnung");
+			double length1, length2;
+
+			System.out.println("Bitte geben Sie die Länge der zwei Seiten als Gleitkommazahl ein.");
+
+			System.out.println("Erste Länge:");
+			length1 = scan.nextDouble();
+			System.out.println("Zweite Länge:");
+			length2 = scan.nextDouble();
+			
+			erg = Math.sqrt( Math.pow(length1, 2) + Math.pow(length2, 2));
+			
 
 		}
 
 		}
 
-		System.out.println("Das Ergebnis ihrer Rechnung ist: " + erg);
+		System.out.println("Das Ergebnis ihrer Rechnung ist: " + String.format("%.2f", erg));
 
 	}
 
