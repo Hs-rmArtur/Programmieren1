1469c579d173df63d5865928f86916d0c39c79cc
Mocren <Kl.Artur@web.de>
2022-11-08 13:10:27 +0100

    Added Aufgabe12

--- Uebung3-Datentypen_Operatoren_II/src/aufgabe11/Main.java
+++ Uebung3-Datentypen_Operatoren_II/src/aufgabe11/Main.java
@@ -8,15 +8,31 @@ public class Main {
 		Scanner scan = new Scanner(System.in);
 		char char1;
 		char char2;
-		
-		
+
+		boolean charBeforeSecondCharInAlphabet, charEqualsSecondChar, charAfterSecondCharInAlphabet,
+				charIsDifferentToSecondChar;
+
 		System.out.println("Geben Sie zwei Buchstaben ein.");
 		char1 = scan.next().charAt(0);
 		char2 = scan.next().charAt(0);
+
+		charBeforeSecondCharInAlphabet = char1 < char2;
+		charEqualsSecondChar = char1 == char2;
+		charAfterSecondCharInAlphabet = char1 > char2;
+
+		charIsDifferentToSecondChar = !charEqualsSecondChar;
+
+		System.out.println("Das Zeichen " + char1 + " steht vor dem Zeichen " + char2 + " im Alphabet: "
+				+ charBeforeSecondCharInAlphabet);
+
+		System.out.println("Das Zeichen " + char1 + " ist gleich dem Zeichen " + char2 + ": " + charEqualsSecondChar);
+
+		System.out.println("Das Zeichen " + char1 + " steht hinter dem Zeichen " + char2 + " im Alphabet: "
+				+ charAfterSecondCharInAlphabet);
 		
-		System.out.println("Das Zeichen " + char1 + " steht vor dem Zeichen " + char2 + " im Alphabet: " + (char1 < char2));
+		System.out.println("Das Zeichen " + char1 + " und " + char2 + " sind verschiedene Zeichen: " + charIsDifferentToSecondChar);
 		
-		System.out.println("Das Zeichen " + char1 + " ist gleich dem Zeichen " + char2 + ": " + (char1 == char2));
+		scan.close();
 
 	}
 
---
+++ Uebung3-Datentypen_Operatoren_II/src/aufgabe12/Main.java
@@ -0,0 +1,34 @@
+package aufgabe12;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int maxChar = Character.MAX_VALUE;
+		int minInt = Integer.MIN_VALUE;
+		int maxInt = Integer.MAX_VALUE;
+		long maxLong = Long.MAX_VALUE;
+		double maxDouble = Double.MAX_VALUE;
+		
+		int overflowInt = maxInt++;
+		long overflowLong = maxLong++;
+		double overflowDouble = maxDouble++;
+
+		System.out.printf("maxChar: %d%nminInt: %d%nmaxInt: %d%nmaxLong: %d%nmaxDouble: %f%n", maxChar, minInt, maxInt,
+				maxLong, maxDouble);
+
+		System.out.printf("Overflow Int: %d%nOverflow Long: %d%nOverflow Double: %f",overflowInt, overflowLong, overflowDouble);
+		
+		System.out.println("Bei Double geht der Wertebereich bei einem Overflow ins unendliche.");
+		
+		
+		char _char = 'a';
+		int _int = 192391;
+		long _long = 193103129;
+		float _float = 1231412.11F;
+		double _double = 123831.5892;
+		
+	
+	}
+	
+}
